aa8330a54a5d00824a4d74e09371dde7f827a40e
Max <max.iw15@yandex.ru>
2023-06-23 16:46:01 +0300

    fix hw3

--- slider/task.js
+++ slider/task.js
@@ -7,16 +7,20 @@ arrowNext.addEventListener("click", clickNext);
 
 // Управление слайдером
 let arr = Array.from(document.querySelectorAll(".slider__item"))
-console.log(arr)
-let active = 0
+
 
 function clickNext () {
+    let active = document.getElementsByClassName('slider__item_active')[0]
+    active = arr.findIndex(elem => elem === active)
+    console.log(active)
     arr[active].classList.remove('slider__item_active')
     active++
     if (active === arr.length) {active = 0}
     arr[active].classList.add('slider__item_active')
 }
 function clickPrev (){
+    let active = document.getElementsByClassName('slider__item_active')[0]
+    active = arr.findIndex(elem => elem === active)
     arr[active].classList.remove('slider__item_active')
     active--
     if (active === -1) {active=arr.length-1}
